f4839fd311c187a0dc24bdc3993bf5d24a4bfc52
Jun An <junimohano@gmail.com>
2017-05-01 22:31:59 -0400

    updated testing

--- JunSpaWebApi.Test/UnitTest1.cs
+++ JunSpaWebApi.Test/UnitTest1.cs
@@ -1,16 +1,42 @@
+using System.Collections.Generic;
+using System.Data.Common;
+using System.IO;
 using JunSpaWebApi.Controllers;
+using JunSpaWebApi.Data;
+using JunSpaWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 using Microsoft.DotNet.PlatformAbstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols;
 
 namespace JunSpaWebApi.Test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public async void Test1()
         {
-            var vc = new ValuesController();
-            Assert.Equal(vc.Test(), "aaa");
+            //var configuration = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory() + "\\..\\..\\..\\..")
+            //    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            //    .Build();
+
+            var options = new DbContextOptionsBuilder<JunSpaContext>()
+                            .UseSqlServer("Server=tcp:junspadbserver.database.windows.net,1433;Initial Catalog=JunSpaDb;Persist Security Info=False;User ID=jun;Password=Qwer1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
+                            .Options;
+
+
+            using (var context = new JunSpaContext(options))
+            {
+                var vc = new BoardsController(context);
+                var okObjResult = (OkObjectResult)await vc.List();
+                var boards = (List<Board>)okObjResult.Value;
+                Assert.Equal(boards.Count, 3);
+            }
 
         }
     }
